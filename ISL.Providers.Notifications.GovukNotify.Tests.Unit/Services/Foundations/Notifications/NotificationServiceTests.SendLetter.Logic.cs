fd9a89da6997f2c047da9c0500259ffc40e00f69
Joe Owens <joe.owens@answerdigital.com>
2025-08-18 16:43:25 +0100

    ShouldSendLetterAsync -> FAIL

---
+++ ISL.Providers.Notifications.GovukNotify.Tests.Unit/Services/Foundations/Notifications/NotificationServiceTests.SendLetter.Logic.cs
@@ -0,0 +1,53 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+
+namespace ISL.Providers.Notifications.GovukNotify.Tests.Unit.Services.Foundations.Notifications
+{
+    public partial class NotificationServiceTests
+    {
+        [Fact]
+        public async Task ShouldSendLetterAsync()
+        {
+            // given
+            string randomIdentifier = GetRandomString();
+            string expectedIdentifier = randomIdentifier;
+            string inputTemplateId = GetRandomString();
+            string inputClientReference = GetRandomString();
+            Dictionary<string, dynamic> inputPersonalization = new Dictionary<string, dynamic>();
+            inputPersonalization.Add("clientReference", inputClientReference);
+
+            this.govukNotifyBroker
+                .Setup(broker =>
+                    broker.SendLetterAsync(
+                        inputTemplateId,
+                        It.Is(SameDictionaryAs(inputPersonalization)),
+                        inputClientReference))
+                .ReturnsAsync(expectedIdentifier);
+
+            // when
+            string actualIdentifier = await this.notificationService.SendLetterAsync(
+                templateId: inputTemplateId,
+                personalisation: inputPersonalization,
+                clientReference: inputClientReference);
+
+            // then
+            actualIdentifier.Should().BeEquivalentTo(expectedIdentifier);
+
+            this.govukNotifyBroker
+                .Verify(broker =>
+                    broker.SendLetterAsync(
+                        inputTemplateId,
+                        It.Is(SameDictionaryAs(inputPersonalization)),
+                        inputClientReference),
+                Times.Once);
+
+            this.govukNotifyBroker.VerifyNoOtherCalls();
+        }
+    }
+}
