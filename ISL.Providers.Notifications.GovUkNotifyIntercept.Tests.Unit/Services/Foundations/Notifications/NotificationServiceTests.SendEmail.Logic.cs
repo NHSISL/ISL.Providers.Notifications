f9acfcb7bfce243fdc9d4b797de692174c4d1ad9
Joe Owens <joe.owens@answerdigital.com>
2025-09-01 16:37:16 +0100

    GovNotifyIntercept SendEmail unit tests

---
+++ ISL.Providers.Notifications.GovUkNotifyIntercept.Tests.Unit/Services/Foundations/Notifications/NotificationServiceTests.SendEmail.Logic.cs
@@ -0,0 +1,65 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using FluentAssertions;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ISL.Providers.Notifications.GovUkNotifyIntercept.Tests.Unit.Services.Foundations.Notifications
+{
+    public partial class NotificationServiceTests
+    {
+        [Fact]
+        public async Task ShouldSendEmailAsync()
+        {
+            // given
+            string randomIdentifier = GetRandomString();
+            string expectedIdentifier = randomIdentifier;
+            string inputTemplateId = GetRandomString();
+            string inputToEmail = GetRandomEmailAddress();
+            string inputClientReference = GetRandomString();
+            string inputSubject = GetRandomString();
+            string inputMessage = GetRandomString();
+            string inputEmailReplyToId = GetRandomString();
+            string inputOneClickUnsubscribeURL = GetRandomString();
+            string interceptingEmail = this.configurations.InterceptingEmail;
+            Dictionary<string, dynamic> inputPersonalization = new Dictionary<string, dynamic>();
+            inputPersonalization.Add("subject", inputSubject);
+            inputPersonalization.Add("message", inputMessage);
+            inputPersonalization.Add("emailReplyToId", inputEmailReplyToId);
+            inputPersonalization.Add("oneClickUnsubscribeURL", inputOneClickUnsubscribeURL);
+
+            this.govukNotifyBroker
+                .Setup(broker =>
+                    broker.SendEmailAsync(
+                        interceptingEmail,
+                        inputTemplateId,
+                        It.Is(SameDictionaryAs(inputPersonalization)),
+                        inputClientReference))
+                .ReturnsAsync(expectedIdentifier);
+
+            // when
+            string actualIdentifier = await this.notificationService.SendEmailAsync(
+                templateId: inputTemplateId,
+                toEmail: inputToEmail,
+                personalisation: inputPersonalization,
+                clientReference: inputClientReference);
+
+            // then
+            actualIdentifier.Should().BeEquivalentTo(expectedIdentifier);
+
+            this.govukNotifyBroker
+                .Verify(broker =>
+                    broker.SendEmailAsync(
+                        interceptingEmail,
+                        inputTemplateId,
+                        It.Is(SameDictionaryAs(inputPersonalization)),
+                        inputClientReference),
+                Times.Once);
+
+            this.govukNotifyBroker.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ ISL.Providers.Notifications.GovUkNotifyIntercept.Tests.Unit/Services/Foundations/Notifications/NotificationServiceTests.SendEmail.Validations.cs
@@ -0,0 +1,187 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using FluentAssertions;
+using ISL.Providers.Notifications.GovUkNotifyIntercept.Models.Foundations.Notifications.Exceptions;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ISL.Providers.Notifications.GovUkNotifyIntercept.Tests.Unit.Services.Foundations.Notifications
+{
+    public partial class NotificationServiceTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldValidateArgumentsOnSendEmailAsync(string invalidText)
+        {
+            // given
+            string inputToEmail = invalidText;
+            string inputTemplateId = invalidText;
+            string inputClientReference = invalidText;
+            Dictionary<string, dynamic> inputPersonalization = null;
+
+            var invalidArgumentNotificationException =
+                new InvalidArgumentNotificationException(
+                    message: "Invalid notification argument exception. Please correct the errors and try again.");
+
+            invalidArgumentNotificationException.AddData(
+                key: "toEmail",
+                values: "Text is required");
+
+            invalidArgumentNotificationException.AddData(
+                key: "templateId",
+                values: "Text is required");
+
+            invalidArgumentNotificationException.AddData(
+                key: "personalisation",
+                values: "Dictionary is required");
+
+            var expectedNotificationValidationException =
+                new NotificationValidationException(
+                    message: "Notification validation error occurred, please correct the errors and try again.",
+                    innerException: invalidArgumentNotificationException);
+
+            // when
+            ValueTask<string> sendEmailTask = this.notificationService.SendEmailAsync(
+                templateId: inputTemplateId,
+                toEmail: inputToEmail,
+                personalisation: inputPersonalization,
+                clientReference: inputClientReference);
+
+            NotificationValidationException actualNotificationValidationException =
+                await Assert.ThrowsAsync<NotificationValidationException>(async () =>
+                    await sendEmailTask);
+
+            // then
+            actualNotificationValidationException.Should()
+                .BeEquivalentTo(expectedNotificationValidationException);
+
+            this.govukNotifyBroker.Verify(broker =>
+                broker.SendEmailAsync(
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<Dictionary<string, dynamic>>(),
+                    It.IsAny<string>()),
+                Times.Never);
+
+            this.govukNotifyBroker.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldValidateDictionaryOnSendEmailAsync()
+        {
+            // given
+            string inputToEmail = GetRandomEmailAddress();
+            string inputTemplateId = GetRandomString();
+            string inputClientReference = GetRandomString();
+            Dictionary<string, dynamic> inputPersonalization = new Dictionary<string, dynamic>();
+
+            var invalidArgumentNotificationException =
+                new InvalidArgumentNotificationException(
+                    message: "Invalid notification argument exception. Please correct the errors and try again.");
+
+            invalidArgumentNotificationException.AddData(
+                key: "message",
+                values: "Text is required for dictionary item");
+
+            invalidArgumentNotificationException.AddData(
+                key: "subject",
+                values: "Text is required for dictionary item");
+
+            var expectedNotificationValidationException =
+                new NotificationValidationException(
+                    message: "Notification validation error occurred, please correct the errors and try again.",
+                    innerException: invalidArgumentNotificationException);
+
+            // when
+            ValueTask<string> sendEmailTask = this.notificationService.SendEmailAsync(
+                templateId: inputTemplateId,
+                toEmail: inputToEmail,
+                personalisation: inputPersonalization,
+                clientReference: inputClientReference);
+
+            NotificationValidationException actualNotificationValidationException =
+                await Assert.ThrowsAsync<NotificationValidationException>(async () =>
+                    await sendEmailTask);
+
+            // then
+            actualNotificationValidationException.Should()
+                .BeEquivalentTo(expectedNotificationValidationException);
+
+            this.govukNotifyBroker.Verify(broker =>
+                broker.SendEmailAsync(
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<Dictionary<string, dynamic>>(),
+                    It.IsAny<string>()),
+                Times.Never);
+
+            this.govukNotifyBroker.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldValidateInterceptingEmailOnSendEmailAsync(string invalidText)
+        {
+            // given
+            string inputToEmail = GetRandomEmailAddress();
+            string inputTemplateId = GetRandomString();
+            string inputClientReference = GetRandomString();
+            string inputSubject = GetRandomString();
+            string inputMessage = GetRandomString();
+            string inputEmailReplyToId = GetRandomString();
+            string inputOneClickUnsubscribeURL = GetRandomString();
+            Dictionary<string, dynamic> inputPersonalization = new Dictionary<string, dynamic>();
+            inputPersonalization.Add("subject", inputSubject);
+            inputPersonalization.Add("message", inputMessage);
+            inputPersonalization.Add("emailReplyToId", inputEmailReplyToId);
+            inputPersonalization.Add("oneClickUnsubscribeURL", inputOneClickUnsubscribeURL);
+
+            this.configurations.InterceptingEmail = invalidText;
+
+            var invalidArgumentNotificationException =
+                new InvalidArgumentNotificationException(
+                    message: "Invalid notification argument exception. Please correct the errors and try again.");
+
+            invalidArgumentNotificationException.AddData(
+                key: "interceptingEmail",
+                values: "Text is required");
+
+            var expectedNotificationValidationException =
+                new NotificationValidationException(
+                    message: "Notification validation error occurred, please correct the errors and try again.",
+                    innerException: invalidArgumentNotificationException);
+
+            // when
+            ValueTask<string> sendEmailTask = this.notificationService.SendEmailAsync(
+                templateId: inputTemplateId,
+                toEmail: inputToEmail,
+                personalisation: inputPersonalization,
+                clientReference: inputClientReference);
+
+            NotificationValidationException actualNotificationValidationException =
+                await Assert.ThrowsAsync<NotificationValidationException>(async () =>
+                    await sendEmailTask);
+
+            // then
+            actualNotificationValidationException.Should()
+                .BeEquivalentTo(expectedNotificationValidationException);
+
+            this.govukNotifyBroker.Verify(broker =>
+                broker.SendEmailAsync(
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<Dictionary<string, dynamic>>(),
+                    It.IsAny<string>()),
+                Times.Never);
+
+            this.govukNotifyBroker.VerifyNoOtherCalls();
+        }
+    }
+}
